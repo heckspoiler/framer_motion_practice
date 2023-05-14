8fb9d336228cc2bb4257e97e598bbc7ae55b7e58
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-14 13:58:31 +0200

    adding visual effects

--- framer_motion_practice/src/components/ImagesBackground/ImagesBackground.js
+++ framer_motion_practice/src/components/ImagesBackground/ImagesBackground.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { motion as m } from "framer-motion";
 import "./ImagesBackground.css";
 import cassette from "./img/cassette.jpeg";
+import wallStripes from "./img/wall-stripes.jpeg";
 
 export const ImagesBackground = () => {
   return (
     <div className="images-background-container">
-      <img src={cassette} alt="cassette" className="cassette" />
+      <m.img src={cassette} alt="cassette" className="cassette" />
+      <m.img src={wallStripes} alt="wall-stripes" className="wall-stripes" />
     </div>
   );
 };
--- framer_motion_practice/src/components/MainTitle/MainTitle.js
+++ framer_motion_practice/src/components/MainTitle/MainTitle.js
@@ -1,13 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { motion as m } from "framer-motion";
 import "./MainTitle.css";
 
+const colors = ["transparent", "#D309E1", "#9C1AFF", "#7700FF", "green"];
+
 export const MainTitle = () => {
+  const [color, setColor] = useState(colors[0]);
+  const [timeoutId, setTimeoutId] = useState(null);
+
+  const startColorChange = () => {
+    let i = 0;
+    const changeColor = () => {
+      setColor(colors[i]);
+      i = (i + 1) % colors.length;
+      setTimeoutId(setTimeout(changeColor, 40));
+    };
+    changeColor();
+  };
+
+  const stopColorChange = () => {
+    clearTimeout(timeoutId);
+    setTimeoutId(null);
+    setColor(colors[0]);
+  };
   return (
     <div className="main-title-section">
       <section className="cover-title-section">
         <m.h1 className="cover-title-section-title empty">
-          EM<span className="empty-p">P</span>TY
+          EM
+          <span
+            className="empty-p"
+            style={{ color }}
+            onMouseEnter={startColorChange}
+            onMouseLeave={stopColorChange}
+          >
+            P
+          </span>
+          TY
         </m.h1>
         <m.h1 className="cover-title-section-title space">SPACE</m.h1>
         <m.h1 className="cover-title-section-title motion">MOTION</m.h1>
