de75e4c7ab61e7e92a505c9e6e1f0be4be53c787
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-17 20:37:20 +0200

    changes

--- framer_motion_practice/src/components/CornerText/CornerText.js
+++ framer_motion_practice/src/components/CornerText/CornerText.js
@@ -4,7 +4,7 @@ import { motion as m } from "framer-motion";
 export const CornerText = () => {
   return (
     <div className="corner-text_container">
-      <p className="corner-text_paragraph">No Weapon Is Absolute</p>
+      <p className="corner-text_paragraph">No Weapon Is ABSOLUTE</p>
     </div>
   );
 };
